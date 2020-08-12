7c32674771cb0e850511a7fa65ace89007891739
hareshshaha7 <haresh.shaha7@gmail.com>
2020-08-13 00:01:17 +0530

    MaxProduct: Find a pair with maximum product in array of Integers

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # gigs-for-gigs
 Problems from Gigs For Gigs
 
-HighestPowerof2: Highest power of 2 less than or equal to given number
-NumberOfPaths: Count all possible paths from top left to bottom right of a mXn matrix
+<br/> HighestPowerof2: Highest power of 2 less than or equal to given number
+<br/> NumberOfPaths: Count all possible paths from top left to bottom right of a mXn matrix
+<br/> MaxProduct: Find a pair with maximum product in array of Integers
---
+++ src/com/haresh/amazon/MaxProduct.java
@@ -0,0 +1,46 @@
+package com.haresh.amazon;
+
+public class MaxProduct {
+	 /* Driver program to test above function */
+    public static void main(String[] args)  
+    { 
+        int arr[] = {1, 4, 3, 6, 7, 0}; 
+        int n = arr.length; 
+        maxProduct(arr, n); 
+              
+    }
+
+	private static void maxProduct(int[] arr, int n) {
+		// TODO Auto-generated method stub
+		int posA = 0;
+		int posB = 0;
+		int negA = 0;
+		int negB = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (posA < arr[i]) {
+				posB = posA;
+				posA = arr[i];
+			}
+			else if (posB < arr[i]) {
+				posB = arr[i];
+			}
+			
+			if (negA > arr[i]) {
+				negB = negA;
+				negA = arr[i];
+			}
+			else if (negB > arr[i]) {
+				negB = arr[i];
+			}
+		}
+
+		if ((posA * posB) > (negA * negB)) {
+			System.out.println("Max product pair is {" + posA + ", " + posB + "}");
+		}
+		else {
+			System.out.println("Max product pair is {" + negA + ", " + negB + "}");				
+		}
+		
+	} 
+}
