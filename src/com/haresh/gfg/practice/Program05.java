52cc35a6663ff59089ce6068653e9415e541f2e4
hareshshaha7 <haresh.shaha7@gmail.com>
2020-12-07 20:12:39 +0530

    Java 8 - Lambda Expression

---
+++ src/com/haresh/gfg/practice/Program05.java
@@ -0,0 +1,76 @@
+package com.haresh.gfg.practice;
+
+interface Xerox {
+	public void print();
+}
+
+interface XeroxMultiply {
+	public void square(int num);
+}
+
+interface Multiply {
+	public int square(int num);
+}
+
+interface Addition {
+	public int sum(int num1, int num2);
+}
+
+public class Program05 {
+
+	public static void main(String[] args) {
+		// Java 7
+		/*		
+		 	class X1 implements Xerox {
+				public void print() {
+					System.out.println("Hello");
+				}
+			}
+		*/
+
+		// Java 8
+		Xerox x1 = () -> System.out.println("Hello");
+
+		// Java 7
+		/*
+			class XMultiplication implements XeroxMultiply {
+				public void square(int num) {
+					System.out.println(num * num);
+				}
+			}
+		*/
+
+		// Java 8
+		XeroxMultiply xMultiplication = (num) -> System.out.println(num * num);
+
+		// Java 7
+		/*
+			class Multiplication implements Multiply {
+				public int square(int num) { 
+					return num * num; 
+				}
+			}
+		*/
+
+		// Java 8
+		Multiply multiplication1 = num -> num * num;
+		// OR
+		Multiply multiplication2 = (num) -> {
+			return num * num;
+		};
+
+		// Java 7
+		/*
+			class Add implements Addition {
+				public int sum(int num1, int num2) {
+					return num1 + num2;
+				}
+			}
+		*/
+
+		// Java 8
+		Addition add = (num1, num2) -> num1 + num2;
+
+	}
+
+}
--- src/com/haresh/gfg/practice/README.md
+++ src/com/haresh/gfg/practice/README.md
@@ -4,5 +4,6 @@
 <br/>
 <br/> <b> Program 01: </b> Mario and the princess
 <br/> <b> Program 02: </b> Inheritance Twister
-<br/> <b> Program 02: </b> Get Reportees
-<br/> <b> Program 02: </b> Java 8 - Functional Programming
+<br/> <b> Program 03: </b> Get Reportees
+<br/> <b> Program 04: </b> Java 8 - Functional Programming
+<br/> <b> Program 05: </b> Java 8 - Lambda Expression through Interfaces
